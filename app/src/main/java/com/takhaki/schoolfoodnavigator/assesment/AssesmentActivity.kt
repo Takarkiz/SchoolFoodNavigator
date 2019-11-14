f092f17041f91246cb95db00f9130a467d07ae20
Takarkiz <ponpontanuki16@gmail.com>
2019-11-15 00:59:38 +0900

    MPAndroidChat導入

--- app/src/main/java/com/takhaki/schoolfoodnavigator/assesment/AssesmentActivity.kt
+++ app/src/main/java/com/takhaki/schoolfoodnavigator/assesment/AssesmentActivity.kt
@@ -1,12 +1,24 @@
 package com.takhaki.schoolfoodnavigator.assesment
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NavUtils
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
+import com.github.mikephil.charting.charts.RadarChart
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.data.*
+import com.github.mikephil.charting.formatter.PercentFormatter
 import com.takhaki.schoolfoodnavigator.R
+import com.takhaki.schoolfoodnavigator.databinding.ActivityAssesmentBinding
+import kotlinx.android.synthetic.main.activity_assesment.*
 
 class AssesmentActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: AssesmentViewModel
+    private lateinit var binding: ActivityAssesmentBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +26,18 @@ class AssesmentActivity : AppCompatActivity() {
         val shopName: String = intent.getCharSequenceExtra("shopName").toString()
         actionBar?.setDisplayShowHomeEnabled(true)
         setTitle(shopName)
+
+        binding = DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_assesment
+        )
+
+        viewModel = ViewModelProviders.of(this).get(AssesmentViewModel::class.java)
+        binding.lifecycleOwner = this
+        binding.assesmentViewModel = viewModel
+
+        setUpRadarChart()
+
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
@@ -25,5 +49,37 @@ class AssesmentActivity : AppCompatActivity() {
             }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    private fun setUpRadarChart() {
+
+        val legend: Legend? = radarChart.legend
+        legend?.horizontalAlignment = Legend.LegendHorizontalAlignment.LEFT
+
+        // データセット
+        val value = listOf(
+            viewModel.goodValue.value?.let { it } ?: 0f,
+            viewModel.distanceValue.value?.let { it } ?: 0f,
+            viewModel.cheepValue.value?.let { it } ?: 0f
+        )
+        val label = listOf("おいしさ", "近さ", "安さ")
+        val entry = ArrayList<RadarEntry>()
+        for (i in value.indices) {
+            entry.add(RadarEntry(value[i], label[i]))
+        }
+
+        /**
+         * ラベル
+         */
+        val dataSet = RadarDataSet(entry, "")
+        dataSet.setDrawValues(false)
+        val radarData = RadarData(dataSet)
+        radarData.setValueFormatter(PercentFormatter())
+        radarData.setValueTextSize(20f)
+        radarData.setValueTextColor(Color.BLACK)
+        radarChart.data = radarData
+        radarChart.data.notifyDataChanged()
+        radarChart.notifyDataSetChanged()
+        radarChart.invalidate()
     }
 }
--- app/src/main/java/com/takhaki/schoolfoodnavigator/assesment/AssesmentViewModel.kt
+++ app/src/main/java/com/takhaki/schoolfoodnavigator/assesment/AssesmentViewModel.kt
@@ -1,7 +1,46 @@
 package com.takhaki.schoolfoodnavigator.assesment
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class AssesmentViewModel(shopId: String) : ViewModel() {
+class AssesmentViewModel : ViewModel() {
+
+    private val _shopId = MutableLiveData<String>()
+    val shopId: LiveData<String>
+        get() = _shopId
+
+    private val _goodValue = MutableLiveData<Float>().apply { value = 0f }
+    val goodValue: LiveData<Float>
+        get() = _goodValue
+
+    private val _distanceValue = MutableLiveData<Float>().apply { value = 0f }
+    val distanceValue: LiveData<Float>
+        get() = _distanceValue
+
+    private val _cheepValue = MutableLiveData<Float>().apply { value = 0f }
+    val cheepValue: LiveData<Float>
+        get() = _cheepValue
+
+    private val _isVisibleFinishButton = MutableLiveData<Boolean>()
+    val isVisibleFinishButton: LiveData<Boolean>
+        get() = _isVisibleFinishButton
+
+
+    fun putShopId(shopId: String) {
+        _shopId.value = shopId
+    }
+
+    fun onUpdateGood(rating: Float) {
+
+    }
+
+    fun onUpdateDistance(rating: Float) {
+
+    }
+
+    fun onUpdateCheep(rating: Float) {
+
+    }
 
 }
