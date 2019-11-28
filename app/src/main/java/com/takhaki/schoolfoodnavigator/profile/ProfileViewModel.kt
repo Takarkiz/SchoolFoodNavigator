d2734e88b07938bfbe9bcac3addda04e9cd64a36
Takarkiz <ponpontanuki16@gmail.com>
2019-11-28 11:57:49 +0900

    プロフィールページに現在の称号を表示する

--- app/src/main/java/com/takhaki/schoolfoodnavigator/profile/ProfileViewModel.kt
+++ app/src/main/java/com/takhaki/schoolfoodnavigator/profile/ProfileViewModel.kt
@@ -16,6 +16,7 @@ class ProfileViewModel : ViewModel() {
 
     val userName = MutableLiveData<String>().apply { value = "ユーザー名" }
     val userPoint = MutableLiveData<Int>().apply { value = 0 }
+    val userGradeTitle = MutableLiveData<String>().apply { value = "一般人" }
 
     fun updateUserProfile(uid: String) {
         _pageUserUid.value = uid
@@ -23,6 +24,7 @@ class ProfileViewModel : ViewModel() {
             userName.value = user.name
             userPoint.value = user.navScore
             _userImageUrl.value = user.profImageUrl
+            userGradeTitle.value = caliculateUserRank(user.navScore)
         }
     }
 
@@ -40,7 +42,17 @@ class ProfileViewModel : ViewModel() {
     }
 
     // ユーザーの称号を計算するメソッド
-    private fun caliculateUserRank() {
+    private fun caliculateUserRank(point: Int): String {
+        return when (point) {
+            in 0..9 -> "一般人"
+            in 10..19 -> "ちょっと詳しい人"
+            in 20..39 -> "グルメな人"
+            in 40..59 -> "通りの達人"
+            in 60..79 -> "グルメ案内人"
+            in 80..100 -> "グルメマスター"
+            else -> "___"
+        }
+
 
     }
 }
