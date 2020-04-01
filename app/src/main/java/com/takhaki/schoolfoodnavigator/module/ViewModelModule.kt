75a733767dcba811002889a7be51406f71f3bbd7
Takarkiz <ponpontanuki16@gmail.com>
2020-04-02 00:11:56 +0900

    viewModelModuleの作成

---
+++ app/src/main/java/com/takhaki/schoolfoodnavigator/module/ViewModelModule.kt
@@ -0,0 +1,32 @@
+package com.takhaki.schoolfoodnavigator.module
+
+import com.takhaki.schoolfoodnavigator.addShop.AddShopViewModel
+import com.takhaki.schoolfoodnavigator.assesment.AssesmentViewModel
+import com.takhaki.schoolfoodnavigator.detail.DetailViewModel
+import com.takhaki.schoolfoodnavigator.mainList.ShopListViewModel
+import com.takhaki.schoolfoodnavigator.profile.ProfileViewModel
+import com.takhaki.schoolfoodnavigator.register.CreateRoomViewModel
+import com.takhaki.schoolfoodnavigator.register.IconRegisterViewModel
+import com.takhaki.schoolfoodnavigator.register.NameRegisterViewModel
+import org.koin.android.ext.koin.androidApplication
+import org.koin.android.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val viewModelModule = module {
+
+    viewModel { ShopListViewModel(androidApplication()) }
+
+    viewModel { AddShopViewModel(androidApplication()) }
+
+    viewModel { AssesmentViewModel(androidApplication()) }
+
+    viewModel { DetailViewModel(androidApplication()) }
+
+    viewModel { ProfileViewModel(androidApplication()) }
+
+    viewModel { CreateRoomViewModel(androidApplication()) }
+
+    viewModel { IconRegisterViewModel(androidApplication()) }
+
+    viewModel { NameRegisterViewModel() }
+}
